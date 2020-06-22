4e3186f60300b5b3a56309280c67150fd8b122ff
l@xkdesktop <xsro@foxmail.com>
2020-06-22 23:23:24 +0800

    修改了coderunner的显示效果

--- ASM/TASMit.bat
+++ ASM/TASMit.bat
@@ -20,43 +20,49 @@ if %1==5 goto deal5 REM 将文件复制到TASM文件夹中，dosbox打开该文件夹
 if %1==6 goto deal6 %3 REM 使用dosbox打开当前文件所在文件夹
 :deal0
     echo output in terminal
-    echo OUTPUT:
+    echo DOSBOX OUTPUT:
     REM -conf %dd%minbox.conf
     %dd%DOSBox -noconsole^
     -c "mount c \"%aa%\"" -c "c:"^
-    -c "tasm/t/zi T.ASM>T.txt" -c "tlink/v/3 T.OBJ>>T.txt"-c "T.EXE>>T.txt"^
+    -c "tasm/t/zi T.ASM>T.txt" ^
+    -c "if exist T.OBJ tlink/v/3 T>>T.txt"^
+    -c "echo ----->>T.txt"^
+    -c "if exist T.EXE T>>T.txt"^
     -c "EXIT"
     echo.>>T.txt
     type T.txt
     exit
 :deal1
     echo output in dosbox,input "exit" or ctrl-F9 or click 'x' to exit dosbox
-    start /min %dd%DOSBox -conf %dd%bigbox.conf ^
+    %dd%DOSBox -noautoexec -noconsole -conf %dd%bigbox.conf ^
     -c "mount c \"%aa%\"" -c "c:"^
     -c "tasm/zi T.ASM" -c "tlink/v/3 T.OBJ"-c "T.EXE"
     exit
 :deal2
     echo output in dosbox ，press any key to exit dosbox
-    %dd%DOSBox -conf %dd%bigbox.conf -noautoexec ^
+    %dd%DOSBox -noautoexec -noconsole -conf %dd%bigbox.conf  ^
     -c "mount c \"%aa%\"" -c "c:"^
     -c "tasm/zi T.ASM" -c "tlink/v/3 T.OBJ"-c "T.EXE"^
     -c "pause" -c "exit"
     exit
 :deal3
     echo tasm and turbo debugger in dosbox
-    copy TDCONFIG1 TDCONFIG.TD REM 使得td.exe 使用这个配置文件
-    %dd%DOSBox -conf %dd%bigbox.conf -noautoexec -c "mount c \"%aa%\"" -c "c:"^
-    -c "tasm/zi T.ASM" -c "tlink/v/3 T.OBJ"-c "Td t"
+    copy TDCONFIG1 TDCONFIG.TD
+    REM 使得td.exe 使用这个配置文件
+    %dd%DOSBox -noautoexec -noconsole -conf %dd%bigbox.conf ^
+    -c "mount c \"%aa%\"" -c "c:"^
+    -c "tasm/zi T.ASM" -c "tlink/v/3 T.OBJ"^
+    -c "Td t"
     exit
 
 :deal5
     echo copy file to TASM folder and dosbox at the folder
-    %dd%DOSBox -conf %dd%bigbox.conf -noautoexec^
+    %dd%DOSBox -noautoexec -noconsole -conf %dd%bigbox.conf ^
     -c "mount c %aa%" -c "c:" -c"dir"
     exit
 :deal6
-    echo add tasm to path and dosbox here
-    %dd%DOSBox -conf %dd%bigbox.conf -noautoexec^
+    echo dosbox here with tasm added to path
+    %dd%DOSBox -noautoexec -noconsole -conf %dd%bigbox.conf ^
     -c "mount d %aa%" -c "set path=D:"^
     -c "mount c '%~d3%~p3'" -c "c:" -c "dir"
     exit
@@ -66,7 +72,7 @@ exit
 if exist TD.TR echo tr exist
     echo turbo debugger without tasm first in dosbox
     copy TDCONFIG1 TDCONFIG.TD
-    %dd%DOSBox -conf %dd%bigbox.conf -noautoexec ^
+    %dd%DOSBox -noautoexec -noconsole -conf %dd%bigbox.conf^
     -c "mount c %aa%" -c "c:" -c "td t"
 echo ---end---
 exit
