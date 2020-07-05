9267530b2b4a0d2e8a972830f266982db93c37dd
lc@kun'sASUS <xsro@foxmail.com>
2020-07-05 15:29:20 +0800

    修复添加MASM支持时引入的BUG

--- ASMtools/ASMit.bat
+++ ASMtools/ASMit.bat
@@ -1,6 +1,8 @@
 @echo off
-REM %1为选择执行命令的方式 %2为工作区根目录 %3为需要操作的文件
+:: GB 2312
+:: %1为选择执行命令的方式 %2为工作区根目录 %3为需要操作的文件
 ::输出编译信息
+if "%1" == "" echo "no invar. please use ASMit.BAT <Mode> <WorkspaceRootPpath> <filepath>" && exit 4
 set  bb=\ASMtools
 set  cc=\ASMtools\Dosbox\
 set "aa=%2%bb%"
@@ -9,11 +11,12 @@ echo Time:%time% WorkspaceRoot:%2
 echo DOSBox frome %dd% ASMtoolsfrom:%aa%
 echo Mode:%1 ASMfilefrom:%3
 ::运行dosbox的准备工作
-cd %aa%/test
+if not exist %aa%\test mkdir %aa%\test
+cd %aa%\test
 set mcd=-noautoexec -noconsole -c "mount c \"%aa%\"" -c "set path=C:\TASM;C:\masm" -c "c:" -c "cd test"
 ::根据%1指令执行相应调试操作
-if %1==4 goto dealA
-if %1==4 goto dealB
+if %1==A goto dealA
+if %1==B goto dealB
 ::进行汇编之前的准备工作，清理临时文件，写入当前文件
 if %~x3==.ASM goto NEXT
 if %~x3==.asm goto NEXT
@@ -23,15 +26,15 @@ exit 1
 if exist T.* del T.*>nul
 copy %3 T.ASM>nul
 ::根据%1指令选择不同的编译运行操作
-if %1==0 goto deal0 REM 将文件复制到Test文件夹中，dosbox打开该文件夹
-if %1==1 goto deal1 REM 在终端中输出结果，code runner没有设置在终端中运行的话会在输出中输出结果
-if %1==2 goto deal2 REM 在dosbox中汇编链接运行
-if %1==3 goto deal3 REM 在dosbox中运行程序，按任意键结束关闭dosbox，对于一些会检测键盘状态的程序，比如chapter 4/OC.asm会自动关闭
-if %1==4 goto deal4 REM 在dosbox中汇编链接调试
-if %1==5 goto deal5 REM 在终端中输出结果，code runner没有设置在终端中运行的话会在输出中输出结果
-if %1==6 goto deal6 REM 在dosbox中汇编链接运行
-if %1==7 goto deal7 REM 在dosbox中运行程序，按任意键结束关闭dosbox，对于一些会检测键盘状态的程序，比如chapter 4/OC.asm会自动关闭
-if %1==8 goto deal8 REM 在dosbox中汇编链接调试
+if %1==0 goto deal0 
+if %1==1 goto deal1 
+if %1==2 goto deal2 
+if %1==3 goto deal3 
+if %1==4 goto deal4 
+if %1==5 goto deal5 
+if %1==6 goto deal6 
+if %1==7 goto deal7 
+if %1==8 goto deal8 
 echo invalid selection %1
 exit 2
 :deal0
@@ -45,13 +48,13 @@ exit 2
     %dd%DOSBox %mcd%^
     -c "tasm/zi T.ASM>>T.txt"^
     -c "if exist T.OBJ tlink/v/3 T>>T.txt"^
-    -c "if exist T.EXE T>R.txt"^
+    -c "if exist T.EXE T>T.out"^
     -c "EXIT"
-    ::输出结果，再前面增加了两个空格
+    ::输出结果，在前面增加了两个空格
     FOR /F "eol=; tokens=* delims=, " %%i in (T.txt) do echo   %%i
     if exist T.EXE echo [YOUR program] OUTPUT:
     ::TODO 显示运行结果，不知道可不可以实现同时打印行号
-    type R.txt
+    FOR /F "eol=; tokens=* delims=, " %%i in (T.out) do echo   %%i
     exit
 :deal2
     echo *Output in dosbox,input "exit" or ctrl-F9 or click 'x' to exit dosbox
@@ -66,8 +69,8 @@ exit 2
     exit
 :deal4
     echo *Tasm and turbo debugger in dosbox
-    cd tasm && copy TDCONFIG1 TDCONFIG.TD
-    REM 使得td.exe 使用这个配置文件
+    cd ../tasm && copy TDCONFIG1 TDCONFIG.TD
+    :: 使得td.exe 使用这个配置文件
     %dd%DOSBox -conf %dd%bigbox.conf %mcd%^
     -c "tasm/zi T.ASM" -c "tlink/v/3 T.OBJ"^
     -c "Td t"
@@ -81,8 +84,8 @@ exit 2
     -c "if exist T.EXE T>T.out"^
     -c "exit"
     FOR /F "eol=; tokens=* delims=, " %%i in (T.txt) do echo   %%i
-    if exist T.EXE echo --[YOUR program] OUTPUT:----
-    type T.out
+    if exist T.EXE echo [YOUR program] OUTPUT:
+    FOR /F "eol=; tokens=* delims=, " %%i in (T.out) do echo   %%i
     exit
 :deal6
     echo *Output in dosbox,input "exit" or ctrl-F9 or click 'x' to exit dosbox
@@ -97,18 +100,20 @@ exit 2
     exit
 :deal8
     echo *Masm and debug in dosbox
-    REM 使得td.exe 使用这个配置文件
+    :: 使得td.exe 使用这个配置文件
     %dd%DOSBox -conf %dd%bigbox.conf %mcd%^
     -c "masm T.ASM;" -c "link T.OBJ;"^
     -c "debug T.EXE"
     exit
 :dealA
-if exist TD.TR echo tr exist
+    if  exist TD.TR echo tr exist
+    if not exist T.EXE  echo no EXE file && exit 3
     echo *Turbo debugger without tasm first in dosbox
-    cd tasm && copy TDCONFIG1 TDCONFIG.TD
+    cd ../tasm && copy TDCONFIG1 TDCONFIG.TD
     %dd%DOSBox -conf %dd%bigbox.conf %mcd%-c "td t"
     exit
 :dealB
+    if not exist T.EXE  echo no EXE file && exit 3
     echo *Masm debugg without tasm first in dosbox
     %dd%DOSBox -conf %dd%bigbox.conf %mcd%-c "debug t.exe"
     exit
